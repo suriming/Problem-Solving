72ab27b4de81a940294fe76de5feab7ea312efdc
suriming <soolim520@gmail.com>
2023-07-22 11:58:43 +0900

    day2 tmp commit

---
+++ sw_dx/day2_1/day2_1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <list>
+using namespace std;
+//암호문 3
+
+int TC = 10;
+int N;
+
+void l_func(){
+
+}
+
+void d_func(){
+
+}
+
+void a_func(){
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    for (int i = 1; i <= TC; ++i){
+        cin >> N;
+    }
+    return 0;
+}
