0d336dda0832492e9769f0072a480e04dae6d15e
suriming <soolim520@gmail.com>
2023-08-20 22:42:43 +0900

    gitignore update

--- daily/2022/November/w2/10171.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-int main(){
-    printf("\\    /\\\n");
-    printf(" )  ( ')\n");
-    printf("(  /  )\n");
-    printf(" \\(__)|\n");
-    return 0;
-}
--- daily/Jan/1152.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-//단어의 개수
-
-int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    cin >>
-}
